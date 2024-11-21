e826369a499e5b41137fb53f6270b73977bb2485
Ash Srinivas <ash@supallama.ai>
2024-11-20 21:22:47 -0800

    Updated app name

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,17 @@
+from crewai import Agent, Task, Crew
+from langchain_openai import ChatOpenAI
 import toga
 
 
 def button_handler(widget):
     print("hello world!")
 
+def initialize_llm():
+    return ChatOpenAI(
+        model="llama3.2",
+        base_url="http://127.0.0.1:8080/",
+        api_key="nokey"
+    )
 
 def build(app):
     box = toga.Box()
@@ -17,7 +25,7 @@ def build(app):
 
 
 def main():
-    return toga.App("First App", "ai.supallama.studio", startup=build)
+    return toga.App("Studio from SupaLlama", "ai.supallama.studio", startup=build)
 
 
 if __name__ == "__main__":
